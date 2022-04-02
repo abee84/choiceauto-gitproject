b17ca6037c99cb28d52064f7b15cedabab29b935
abee84 <abeeshsdsd@gmail.com>
2022-04-02 10:15:31 +0530

    completed Teams Model

--- choiceauto/settings.py
+++ choiceauto/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'choiceauto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'choiceauto_db',
+        'USER': 'postgres',
+        'PASSWORD': 'arun95671',
+        'HOST': 'localhost',
     }
 }
 
@@ -122,6 +125,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'choiceauto/static'),
 ]
+
+#media settings
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
